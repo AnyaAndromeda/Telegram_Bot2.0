52d2f3f7ff0de386800f3c7ce3f05eac55a9adb2
AnyaAndromeda <168111013+AnyaAndromeda@users.noreply.github.com>
2024-06-30 16:44:59 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = '6909677861:AAFtvcV0Sw6EtEvSDo7cIDTkcmuxOterF9M'
+
+api_key = 'd521da0ddba9fbb61b6dd7a27f9eb24086c481fe89cdb4dc47cc7a5e99927554'
+
+keys = {
+    'доллар': 'USD',
+    'йена': 'JPY',
+    'евро': 'EUR',
+    'Рубль': 'RUB',
+    'Юань': 'CNY',
+    'Белорусский рубль': 'BYN',
+}
